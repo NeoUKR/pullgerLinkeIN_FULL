b180d0063a518bf2bb71ed5c861a85b6ca93aac1
NeoUKR <neokkv@gmail.com>
2022-10-07 16:35:40 +0300

    --

--- pullgerReflection/com_linkedin__REST/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class com_linkedinConfig(AppConfig):
-    name = 'pullgerReflection.com_linkedin__REST'
-    default_auto_field = 'django.db.models.BigAutoField'
